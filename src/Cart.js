ac5355a254fe0206f40edbdbe756daa1c8714441
Rajat Uba <rajatuba9783@gmail.com>
2021-07-22 15:52:19 +0530

    Rendering array using map

--- src/Cart.js
+++ src/Cart.js
@@ -3,8 +3,12 @@ import CartItem from './CartItem';
 
 class Cart extends React.Component{
     render(){
+        const arr=[1,2,3,4,5]
         return(
             <div className="cart">
+                {arr.map((item)=>{
+                    return item+5
+                })}
                 <CartItem/>
                 <CartItem/>
                 <CartItem/>
